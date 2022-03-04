b8aa489d6bd934e9faed2819107f91627b860344
anjeehye <anjeehye1@gmail.com>
2022-03-04 21:53:44 +0100

    Attempt fixing swup disqus issue

--- assets/script.js
+++ assets/script.js
@@ -25,6 +25,7 @@ const activeFunctions = () => {
   loop();
   jeehyeAudio();
   styleComments();
+  disqusForSwup();
 };
 const resizeFunctions = () => {
   photoMasonryGrid();
@@ -227,3 +228,22 @@ let styleComments = () => {
   }
 };
 styleComments();
+
+let disqusForSwup = () => {
+  let disqusComments = querySelector('#disqus_thread');
+  if(disqusComments) {
+    var disqus_config = function () {
+      this.page.url = '{{ page.url | absolute_url }}';
+      this.page.identifier = '{{ page.url | absolute_url }}';
+    };
+  
+    (function() {
+      var d = document, s = d.createElement('script');
+  
+      s.src = 'https://{{ site.disqus.shortname }}.disqus.com/embed.js';
+  
+      s.setAttribute('data-timestamp', +new Date());
+      (d.head || d.body).appendChild(s);
+    })();
+  }
+};
